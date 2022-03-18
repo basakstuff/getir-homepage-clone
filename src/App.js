196f73776c8ca16873d70d082864a288d50ac3a3
basak er <basaker98@gmail.com>
2022-03-18 16:08:31 +0300

    homepage components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import Header from 'components/Header'
+import HeroSection from 'components/HeroSection';
+import Categories from 'components/Categories';
+import Campaigns from 'components/Campaigns';
+import Favorites from 'components/Favorites';
+import MobileApp from 'components/MobileApp';
+import Cards from 'components/Cards';
+import Footer from 'components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-     test
-    </div>
-  );
+
+	return (
+		<>
+			<Header />
+			<HeroSection />
+			<Categories />
+			<div className="grid gap-y-6 py-6 pb-14 container mx-auto">
+				<Campaigns />
+				<Favorites />
+				<MobileApp />
+				<Cards />
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
